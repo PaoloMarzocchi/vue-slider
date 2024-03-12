b49633aa4382979e1014b939c7764e976c66bed7
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-12 16:33:18 +0100

    add dynamic layout and change of images clicking on buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,38 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            activeImg: 0,
+            series:{
+                title: 'Rick and Morty',
+                genre: 'Animated Fiction',
+                numbSeasons: 7,
+                creators: 'Justin Roiland & Dan Harmon',
+                images: [
+                    "./assets/img/01.jpg",
+                    "./assets/img/02.jpg",
+                    "./assets/img/03.jpg",
+                    "./assets/img/04.jpg",
+                    "./assets/img/05.jpg",
+                    "./assets/img/06.jpg",
+                ]
+            }
+        }
+    },
+    methods: {
+        prev(){
+            this.activeImg--;
             
+
+            if(this.activeImg < 0){
+                this.activeImg = this.series.images.length - 1;
+            }
+        },
+        next(){
+            this.activeImg++;
+            
+            if (this.activeImg === this.series.images.length) {
+                this.activeImg = 0;
+            }
         }
     }
 }).mount('#app')
